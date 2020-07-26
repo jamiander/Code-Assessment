c108dcc84c46abe8060ba6425bbd685e45284241
Jamie Andersen <jamieandersen29@gmail.com>
2020-07-26 16:46:51 -0500

    map working with legend and title

--- kc-commuters-app/src/app/map/map.component.ts
+++ kc-commuters-app/src/app/map/map.component.ts
@@ -21,11 +21,30 @@ export class MapComponent implements OnInit {
     mapboxgl.accessToken = environment.mapbox.accessToken;
     this.map = new mapboxgl.Map({
           container: 'map',
-          style: this.style,
+          style: 'mapbox://styles/jamiander/ckcqo45ht02j91imjgie6lzok',
           zoom: 9,
           center: [-94.57, 39.09],
         });
     this.map.addControl(new mapboxgl.NavigationControl());
+
+    const layers = ['kc-tracts', 'kc-neighborhoods'];
+    const colors = ['#4d65d1', '#d13d3d'];
+
+    for (let i = 0; i < layers.length; i++) {
+      const legend = document.getElementById('legend');
+      const layer = layers[i];
+      const color = colors[i];
+      const item = document.createElement('div');
+      const key = document.createElement('span');
+      key.className = 'legend-key';
+      key.style.backgroundColor = color;
+
+      const value = document.createElement('span');
+      value.innerHTML = layer;
+      item.appendChild(key);
+      item.appendChild(value);
+      legend.appendChild(item);
+    }
 
   }
 
